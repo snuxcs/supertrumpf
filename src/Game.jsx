60cba7de170e1aa874629d3e4876ea3736ca3420
snuxcs <60112252+snuxcs@users.noreply.github.com>
2021-01-07 17:39:35 +0100

    Added all Game Logic -> Finished Logic S. 113

--- src/Game.jsx
+++ src/Game.jsx
@@ -17,18 +17,86 @@ export default class Game extends React.Component {
             computerUncovered: false,
             selectedProperty: '',
             playersTurn: true,
-            player: [new Animal('Elefant', 'placeholder.jpg', 3.3, 6000, 70, 1, 40)],
-            computer: [new Animal('Nashorn', 'placeholder.jpg', 1, 9, 2300, 50, 1, 50)],
+
+            player: [new Animal('Elefant', 'placeholder.jpg', 3.3, 6000, 70, 1, 40),
+                new Animal('Flusspferd', 'placeholder.jpg', 1.5, 1800, 50, 1, 30)],
+
+            computer: [new Animal('Nashorn', 'placeholder.jpg', 1, 9, 2300, 50, 1, 50),
+                new Animal ('Krokodil', 'placeholder.jpg', 5.2, 1000, 70, 60, 29)],
         };
-    getSelectedPropertyHandler() {
-        return property =>
-            this.setState((state) =>
-                update(this.state, {
-                    selectedProperty: {$set: property },
-                    computerUncovered: { $set: true },
-                    }),
-                );
+
+    getSelectPropertyHandler() {
+        return property => this.play(property);
     }
+
+    compare(property) {
+        let playersTurn = this.state.playersTurn;
+
+        const firstPlayer = this.state.player[0];
+        let player = update(this.state.player, { $splice: [[0, 1]] });
+
+        const firstComputer = this.state.computer[0];
+        let computer = update(this.state.computer, { $splice: [[0, 1]] });
+
+        if (firstPlayer[property]>firstComputer[property]){
+            playersTurn = true;
+            player = update(player, {$push: [firstPlayer, firstComputer] });
+            if(computer.length === 0){
+                alert('Player wins'); return;
+            }
+        } else if(firstPlayer[property] < firstComputer[property]) {
+            playersTurn = false;
+            computer = update(computer, { $push: [firstPlayer] });
+            if (player.length===0){
+                alert('Computer wins'); return;
+            }
+        } else {
+            player = update(player, { $push: [firstPlayer] });
+            computer = update(computer, { $push: [firstComputer] });
+        }
+        this.setState(
+            state =>
+                update(state, {
+                    $set: {
+                        computerUncovered: false,
+                        selectedProperty: '',
+                        playersTurn,
+                        player,
+                        computer,
+                    },
+                }),
+                () => {
+                if(!playersTurn){
+                    setTimeout(()=> {
+                        const property = this.selectRandomProperty();
+                        this.play(property);
+                    }, 2000);
+                }
+            },
+        );
+    }
+
+    play(property) {
+        this.setState(
+            state =>
+                update(this.state, {
+                    selectedProperty: { $set: property },
+                    computerUncovered: { $set: true },
+                }),
+            () => {
+                setTimeout(() => {
+                    this.compare(property);
+                }, 2000);
+            },
+        );
+    }
+
+    selectRandomProperty() {
+        const properties = Object.keys(Animal.properties);
+        const index = Math.floor(Math.random()* properties.length);
+        return properties[index];
+    }
+
     render() {
         const { playersTurn, player, computer, selectedProperty, computerUncovered } = this.state;
         return(
@@ -38,7 +106,7 @@ export default class Game extends React.Component {
                     {playersTurn ? 'Du bist ' : 'Der Computer ist'} an der Reihe
                 </div>
                 <div className="cards">
-                    <Card animal={player[0]} uncovered={true} selectedProperty={selectedProperty} onSelectProperty={this.getSelectedPropertyHandler()}/>
+                    <Card animal={player[0]} uncovered={true} selectedProperty={selectedProperty} onSelectProperty={this.getSelectPropertyHandler()}/>
                     <Card animal={computer[0]} uncovered={computerUncovered} selectedProperty={selectedProperty}/>
                 </div>
             </div>
